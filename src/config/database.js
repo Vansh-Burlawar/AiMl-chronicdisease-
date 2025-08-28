200f7257dfe3288381c89b474945fb293f8e5972
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2025-08-28 09:14:34 +0530

    DataBase Connection Establishment

---
+++ src/config/database.js
@@ -0,0 +1,35 @@
+import mysql from 'mysq12/promise';
+
+const dbConfig ={
+    host: import.meta.env.VITE_DB_HOST || 'localhost',
+    user: import.meta.env.VITE_DB_USER || 'root',
+    password: import.meta.env.VITE_DB_PASSWORD || '',
+    database:  import.meta.env.VITE_DB_NAME || 'medical_app',
+    port: import.meta.env.VITE_DB_PORT || 3306,
+
+};
+
+let connection=null;
+export const connectDB = async()=> {
+    try{
+        if(!connection){
+            connection= await mysql.createConnection(dbConfig);
+            console.log('Connected to DataBase')
+        }
+        return connection;
+    }catch(error){
+        console.log("Connection faield",error);
+        throw error;
+    }
+};
+
+
+export const closeDB = async()=> {
+    if(connection){
+        await connection.end();
+        connection= null;
+        console.log('Connection Ended');
+    }
+};
+
+export default connection;
