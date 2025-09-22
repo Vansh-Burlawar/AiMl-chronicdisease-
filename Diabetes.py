8819f192a67aeef4895c0e7e2260a46b175142b1
TejalK-Hub <tejal5kunjir@gmail.com>
2025-09-22 20:27:29 +0530

    Added Diabetes pkl file

---
+++ Diabetes.py
@@ -0,0 +1,10 @@
+import joblib
+import numpy as np
+from flask import flask,jsonify,request
+
+
+app=flask(__name__)
+model = joblib.load(Diabetes.pkl)
+@app.rout("\predict" , methods=["POST"])
+def prefict():
+    
