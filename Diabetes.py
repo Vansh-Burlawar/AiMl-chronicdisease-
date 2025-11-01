ca30cb5b1ef92c135c5c862c21bb4112238a87bf
Vansh-Burlawar <burlawarvansh2002@gmail.com>
2025-11-01 23:48:24 +0530

    Debugging

--- Diabetes.py
+++ Diabetes.py
@@ -4,7 +4,7 @@ from flask import flask,jsonify,request
 
 
 app=flask(__name__)
-model = joblib.load(Diabetes.pkl)
+model = joblib.load("Diabetes.pkl")
 @app.rout("\predict" , methods=["POST"])
 def prefict():
     
